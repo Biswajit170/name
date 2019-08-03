bb36d7578696848bec7218a67cfa522607d32923
Biswajit170 <53347738+Biswajit170@users.noreply.github.com>
2019-08-03 12:03:26 +0530

    Add files via upload

---
+++ ConnectDB.java
@@ -0,0 +1,42 @@
+
+
+// Biswajit Patasani
+// Regd. No : 170301120170
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.SQLException;
+
+public class ConnectDB {
+
+	
+ public static Connection dbConn() throws IOException,SQLException,ClassNotFoundException
+ {
+	 Connection cn;
+     Class.forName("oracle.jdbc.driver.OracleDriver");
+     cn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","cool");
+     
+     return cn;
+ }	
+	public static void main(String[] args) {
+		 try{
+	            
+	            System.out.println(dbConn());
+	            if(dbConn() != null)
+	            {
+	                System.out.println("Connected");
+	            }
+	            else
+	            {
+	                System.err.println("Not Connceted");
+	            }
+	            
+	        }catch( Exception e)
+	        {
+	          e.printStackTrace();  
+	        }  
+		
+	}
+
+}
